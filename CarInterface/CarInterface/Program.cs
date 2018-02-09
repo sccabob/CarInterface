7511785aa9482b19f55c30f866eb6de9be741e64
sccabob <bobholahan@gmail.com>
2018-02-08 20:47:17 -0500

    Work was started in Program.cs to implement a command line menu to select the type of transmission to install in a vehicle.

--- CarInterface/CarInterface/Program.cs
+++ CarInterface/CarInterface/Program.cs
@@ -9,7 +9,12 @@ namespace CarInterface
     class Program
     {
         static void Main(string[] args)
-        {          
+        {
+            //Console.WriteLine("What type of Transmission would like in your Vehicle");
+
+
+
+
             Vehicle myVehicle = new Vehicle(new ManualTransmission());
 
             Console.WriteLine("This vehicle is equipped with a transmission of type " + myVehicle.TransmissionType.ToString());
